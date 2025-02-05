c1aefcfc26d9ddf0027e4eeebcd1485e3773c116
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2025-02-05 10:08:37 +0700

    add

--- learn1/app.duongvct/src/main/java/vn/app/duongvct/config/SecurityConfiguration.java
+++ learn1/app.duongvct/src/main/java/vn/app/duongvct/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import vn.app.duongvct.service.CustomUserDetailsService;
 import vn.app.duongvct.service.UserService;
 
@@ -21,6 +22,7 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public UserDetailsService userDetailsService(UserService userService) {
         return new CustomUserDetailsService(userService);
@@ -36,19 +38,30 @@ public class SecurityConfiguration {
         // authProvider.setHideUserNotFoundExceptions(false);
         return authProvider;
     }
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize.dispatcherTypeMatchers(DispatcherType.FORWARD,
-                                DispatcherType.INCLUDE).permitAll()
-                        .requestMatchers("/", "/login", "/client/**", "/css/**", "/js/**", "/images/**")
-                        .permitAll().anyRequest().authenticated())
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .failureUrl("/login?error")
-                        .permitAll());
-        return http.build();
+    public AuthenticationSuccessHandler customSuccessHandler(){
+        return new CustomSuccessHandler();
     }
 
 
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // v6 lamda
+        http
+                .authorizeHttpRequests(authorize -> authorize.dispatcherTypeMatchers(DispatcherType.FORWARD,
+                                DispatcherType.INCLUDE).permitAll()
+                        .requestMatchers("/", "/login", "/product/**", "/client/**", "/css/**", "/js/**", "/images/**")
+                        .permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().permitAll())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .failureUrl("/login?error")
+                        .successHandler(
+                                customSuccessHandler())
+                        .permitAll());
+        return http.build();
+    }
 }
+
